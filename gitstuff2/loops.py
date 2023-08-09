50b97062c2256a86759f350ac443862482341ae7
praveenavaishnavi <praveenavaishnavi.n@gmail.com>
2023-08-09 17:29:01 +0300

    Remove_Gitstuff2

--- gitstuff2/loops.py
+++
@@ -1,7 +0,0 @@
-greeting = "good morning"
-if greeting == "good morning":
-    print("condition match")
-else:
-    print("condition does not matches")
-    print("second line")
-print("if else condition code is completed")
