48b1a5ff8dbcd26c9d18d30446e95525226b3c0d
praveenavaishnavi <praveenavaishnavi.n@gmail.com>
2023-08-09 17:25:06 +0300

    Cell_value_change1

--- excelDemo.py
+++ excelDemo.py
@@ -1,5 +1,5 @@
 import openpyxl
 book=openpyxl.load_workbook("C:\Users\User\Documents\python-demo.xlsx")
 sheet=book.active
-cell= sheet.cell(row=1,column=2)
+cell= sheet.cell(row=1,column=3)
 print(cell.value)
